303d7ff06f26ad17b2bbb4a16b5535de3895e6ae
Diego López <diego.lopezgarcia@hotmail.com>
2023-10-03 13:20:43 +0200

    feat: Added global CSS, global layout with homepage and navbar and a layout for each post

---
+++ eleventy.config.js
@@ -0,0 +1,50 @@
+module.exports = function (eleventyConfig) {
+  // Copy the contents of the `public` folder to the output folder
+  // For example, `./public/css/` ends up in `_site/css/`
+  eleventyConfig.addPassthroughCopy({
+    "./public/": "/",
+  });
+
+  // Run Eleventy when these files change:
+  // https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
+
+  // Watch content images for the image pipeline.
+  eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpeg}");
+
+  eleventyConfig.addFilter("postTags", (tags) =>
+    Object.keys(tags)
+      .filter((k) => k !== "posts")
+      .filter((k) => k !== "all")
+      .map((k) => ({ name: k, count: tags[k].length }))
+      .sort((a, b) => b.count - a.count)
+  );
+
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: ["md", "njk", "html", "liquid"],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
+    // These are all optional:
+    dir: {
+      input: "src", // default: "."
+    },
+
+    // -----------------------------------------------------------------
+    // Optional items:
+    // -----------------------------------------------------------------
+
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
+
+    // When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
+    // it will transform any absolute URLs in your HTML to include this
+    // folder name and does **not** affect where things go in the output folder.
+    pathPrefix: "/",
+  };
+};
---
+++ src/src.11tydata.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // Set a default layout for everything in the src folder and below.
+  layout: "layouts/default.liquid",
+};
